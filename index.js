428688ec6c03b56c7e092d1e552a4c2c33348371
dwivediji425 <dwivediji425@gmail.com>
2025-04-17 16:08:51 +0530

    cors update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const aiRoute = require('./Routes/aiRoute');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +9,7 @@ const PORT = 3000;
 // Middleware to parse JSON and form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // Serve static files (e.g., CSS, JS, images)
 app.use(express.static('Public'));
